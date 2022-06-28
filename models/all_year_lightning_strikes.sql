28945b640949da7a92823b35ddb14a4f07fcc303
nicholas.huntington <nicholas.huntington@utah.edu>
2022-06-27 19:24:45 -0600

    update all_lightning query

--- models/all_year_lightning_strikes.sql
+++ models/all_year_lightning_strikes.sql
@@ -8,6 +8,22 @@ with all_year_lightning_strikes as (
     union all
 
     select * from `bigquery-public-data`.noaa_lightning.lightning_1989
+
+    union all
+
+    select * from `bigquery-public-data`.noaa_lightning.lightning_1990
+
+    union all
+
+    select * from `bigquery-public-data`.noaa_lightning.lightning_1991
+
+    union all
+
+    select * from `bigquery-public-data`.noaa_lightning.lightning_1992
+
+    union all
+
+    select * from `bigquery-public-data`.noaa_lightning.lightning_1993
 )
 
 select * from all_year_lightning_strikes
